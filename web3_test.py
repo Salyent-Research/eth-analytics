c458f89c7bf8ec7a6b495ae56871e164c8fcc099
Brody <brodyuehara@gmail.com>
2021-11-09 06:56:55 -1000

    Calculating ETH value of a block

--- web3_test.py
+++ web3_test.py
@@ -1,3 +1,4 @@
+import numpy as np
 from web3 import Web3
 from decouple import config
 from datetime import datetime
@@ -14,16 +15,20 @@ latest = web3.eth.blockNumber
 #for i in range(0, 10):
 #  print(web3.eth.getBlock(latest - i))
 print("Block Number: {}".format(latest))
-block_latest = web3.eth.get_block(latest)
+block_latest = web3.eth.get_block(latest-1)
 transactions = block_latest["transactions"]
 len_trans = len(transactions)
-print("ETH Latest Block Numer: {}".format(block_latest["number"]))
+print("ETH Latest Block Number: {}".format(block_latest["number"]))
 print("Number of transaction in block: {}".format(len_trans))
 timestamp = block_latest["timestamp"]
 print("Time: {}".format(datetime.fromtimestamp(timestamp)))
 print("-------"*5)
-print("Block Content: {}".format(block_latest))
-
-
+wei = 0
+for transaction in transactions:
+    # Need to convert bytes to hexbytes using .hex() method
+    info = web3.eth.get_transaction(transaction.hex())
+    wei += info["value"]
+eth = wei / 1000000000000000000
+print("Total Value of Block: {}".format(eth))
 
 
